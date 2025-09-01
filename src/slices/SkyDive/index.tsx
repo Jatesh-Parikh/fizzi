389bf143c993f5d9ba2332c32db5f71a4a075abc
Jatesh Parikh <jateshparikh97@gmail.com>
2025-09-01 22:14:29 +0530

    skydive slice added

--- src/slices/SkyDive/index.tsx
+++ src/slices/SkyDive/index.tsx
@@ -1,7 +1,12 @@
+"use client";
+
 import { FC } from "react";
 import { Content } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
 
+import { Bounded } from "@/components/Bounded";
+import Scene from "./Scene";
+import { View } from "@react-three/drei";
 /**
  * Props for `SkyDive`.
  */
@@ -12,40 +17,19 @@ export type SkyDiveProps = SliceComponentProps<Content.SkyDiveSlice>;
  */
 const SkyDive: FC<SkyDiveProps> = ({ slice }) => {
   return (
-    <section
+    <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      className="skydive h-screen"
     >
-      Placeholder component for sky_dive (variation: {slice.variation}) slices.
-      <br />
-      <strong>You can edit this slice directly in your code editor.</strong>
-      {/**
-       * 💡 Use Prismic MCP with your code editor
-       *
-       * Get AI-powered help to build your slice components — based on your actual model.
-       *
-       * ▶️ Setup:
-       * 1. Add a new MCP Server in your code editor:
-       *
-       * {
-       *   "mcpServers": {
-       *     "Prismic MCP": {
-       *       "command": "npx",
-       *       "args": ["-y", "@prismicio/mcp-server@latest"]
-       *     }
-       *   }
-       * }
-       *
-       * 2. Select a model optimized for coding (e.g. Claude 3.7 Sonnet or similar)
-       *
-       * ✅ Then open your slice file and ask your code editor:
-       *    "Code this slice"
-       *
-       * Your code editor reads your slice model and helps you code faster ⚡
-       * 🎙️ Give your feedback: https://community.prismic.io/t/help-us-shape-the-future-of-slice-creation/19505
-       * 📚 Documentation: https://prismic.io/docs/ai#code-with-prismics-mcp-server
-       */}
-    </section>
+      <h2 className="sr-only">{slice.primary.sentence}</h2>
+      <View className="h-screen w-screen">
+        <Scene
+          flavour={slice.primary.flavour}
+          sentence={slice.primary.sentence}
+        />
+      </View>
+    </Bounded>
   );
 };
 
